cb6d9f82a464d2e5fdded36f6dfc660b9c8f31ad
Shanghai Wu <18620800179@163.com>
2019-05-10 15:51:56 +0800

    管理输出 - 设定HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: { // 多个js入口
     app: './src/index.js',
     print: './src/print.js'
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: '管理输出'
+    })
+  ],
   output: {
     filename: '[name].bundle.js', // [name]最终会被替换成entry的key
     path: path.resolve(__dirname, 'dist')
