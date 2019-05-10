2de0b62abf8e32921f6d8b41678ea0e6d1f1a6b6
Shanghai Wu <18620800179@163.com>
2019-05-10 11:42:59 +0800

    起步 - 使用一个配置文件

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist') // Node.js 中，__dirname 总是指向被执行 js 文件的绝对路径
+  }
+}
