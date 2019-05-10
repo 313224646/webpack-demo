3f6e35655ce22618b3650565a618b211078d121c
Shanghai Wu <18620800179@163.com>
2019-05-10 15:47:20 +0800

    管理输出 - 预先准备

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path = require('path')
 
 module.exports = {
-  entry: './src/index.js',
+  entry: { // 多个js入口
+    app: './src/index.js',
+    print: './src/print.js'
+  },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist') // Node.js 中，__dirname 总是指向被执行 js 文件的绝对路径
+    filename: '[name].bundle.js', // [name]最终会被替换成entry的key
+    path: path.resolve(__dirname, 'dist')
   }
 }
