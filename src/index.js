fcaa9b5b90d06ee10431e61fedae1d9c8f88bf19
Shanghai Wu <18620800179@163.com>
2019-05-10 13:57:42 +0800

    资源管理 - 加载图片

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'loadsh'
 import './style.css'
+import Icon from './icon.png' // 会被file-loader处理
 
 function component () {
   var element = document.createElement('div')
@@ -8,6 +9,12 @@ function component () {
   // 通过npm模块引入，由隐式变为显式，移除dist/index.html对应script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ')
   element.classList.add('hello')
+
+  // 将图像添加到我们现有的 div。
+  var myIcon = new Image()
+  myIcon.src = Icon
+
+  element.appendChild(myIcon)
 
   return element
 }
