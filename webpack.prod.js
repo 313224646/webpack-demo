3beb366f2a1c60228d1f9fe0b3d2fc62514a73e9
Shanghai Wu <18620800179@163.com>
2019-05-13 10:53:17 +0800

    生产环境构建 - source map

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,7 +3,10 @@ const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
+  devtool: 'source-map', // 控制构建的速度与结果，具体选择参考：https://www.webpackjs.com/configuration/devtool/
   plugins: [
-    new UglifyJSPlugin()
+    new UglifyJSPlugin({
+      sourceMap: true
+    })
   ]
 })
