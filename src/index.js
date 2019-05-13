e9b031c569d7d168808cd25eee9879e88519758f
Shanghai Wu <18620800179@163.com>
2019-05-13 13:37:14 +0800

    生成环境构建 - 指定环境

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
-/**
- * 本小节主要展示通过tree shaking清除无用代码
- * 但是根据demo执行发现，无论是否在package.json使用sideEffects: false，还是在webpack.config.js使用mode: "production"
- * 构建的bundle.js都是一样
- * 也许最新的webpack已经做了一些处理，但是文档没有更新，我也没能弄懂它。
- * 这次commit包含整个tree shaking内容，但它不太可靠。
- */
 import { cube } from './math.js'
+
+if (process.env.NODE_ENV !== 'production') {
+  console.log('Looks like we are in development mode!')
+}
 
 function component () {
   var element = document.createElement('pre')
